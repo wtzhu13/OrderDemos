ae4a2a75f04223ae05477e528913d903b13e384b
wtzhu13 <wtzhu_13@163.com>
2020-06-21 22:18:06 +0800

    新建一个商品信息系统

---
+++ Python成绩管理系统（精简版）/Python成绩管理系统（精简版）/main.py
@@ -0,0 +1,7 @@
+from tkinter import *  
+from LoginPage import *  
+  
+root = Tk()  
+root.title('车辆管理系统_by vision')
+LoginPage(root)  
+root.mainloop()  
---
+++ goodsManageSys/manage.h
@@ -0,0 +1,32 @@
+#ifndef _MANAGE_H_
+#define _MANAGE_H_
+
+#include "global.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ui.h"
+
+void initSys();
+void initList();
+void getGoodsNum();
+void addGood();
+
+
+typedef struct Good
+{
+    char name[13];
+    int price;
+    int saleVolume;
+}Good;
+
+Good **goodList;
+int goodsNum;
+void saveDataToFile();
+void showGoodList();
+void delGood();
+int isExit(char *goodName);
+void sortGoodList();
+void sortByPrice();
+
+#endif
